7f5f2813faf740a5e9c7987b5255ba46cd889fb7
David Boedigheimer <stealth259@gmail.com>
2019-11-05 14:46:18 -0800

    Add third pass, adds way to the end of a word with a vowel.

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,24 @@
 // Back end
 var pigTrans = function(words) {
   var vowels = ['a', 'e', 'i', 'o', 'u'];
-  var returnWord = "";
+  var returnWord = ""
+  var sentence = words.split("");
+
   if ((/[a-z]/i).test(words)) {
-    vowels.forEach(function(vowel) {
-      if (vowel === words) {
-        returnWord = words + "way"
-      }
-    });
-    return returnWord    
+    for (var i = 0; i < sentence.length; i++) {
+      vowels.forEach(function(vowel) {
+        if (sentence[i] === vowel) {
+          console.log(sentence.join("") + "way");
+
+          returnWord = sentence.join("") + "way";
+        }
+      });
+    }
+
+    return returnWord
+
   } else {
-    return "number"
+    return "Not a letter"
   }
 }
 // Front end
