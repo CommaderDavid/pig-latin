7999d9577b67f8246700f727c5279fbfb74d2536
David Boedigheimer <stealth259@gmail.com>
2019-11-06 12:03:17 -0800

    Add pass, work with a sentence.

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,33 @@
 // Back end
 var pigTrans = function(words) {
   var vowels = ['a', 'e', 'i', 'o', 'u'];
-  var returnWord = ""
-  var sentence = words.split("");
+  var returnWord = []
+  var splitSentences = words.split(" ");
 
-  if ((/[a-z]/i).test(words)) {
-    for (var i = 0; i < sentence.length; i++) {
-      vowels.forEach(function(vowel) {
-         if (sentence[i] === vowel) {
-           if (i === 0) {
-             returnWord = sentence.join("") + "way";
-           } else {
-             var letters = sentence.splice(0, i);
-             returnWord = sentence.join("") + letters.join("") + "ay"
-             console.log(returnWord, letters, sentence);
+  splitSentences.forEach(function(splitSentence) {
+    var sentence = splitSentence.split("");
+
+    if ((/[a-z]/i).test(words)) {
+      for (var i = 0; i < sentence.length; i++) {
+        vowels.forEach(function(vowel) {
+           if (sentence[i] === vowel) {
+             if (i === 0) {
+               returnWord.push(sentence.join("") + "way ");
+             } else {
+               var letters = sentence.splice(0, i);
+               returnWord.push(sentence.join("") + letters.join("") + "ay ");
+             }
+             i = sentence.length;
            }
-           i = sentence.length;
-         }
-      });
-    }
-    return returnWord
+        });
+      }
 
-  } else {
-    return "Not a letter"
-  }
+    } else {
+      return "Not a letter"
+    }
+
+  });
+  return returnWord;
 }
 // Front end
 $(document).ready(function() {
